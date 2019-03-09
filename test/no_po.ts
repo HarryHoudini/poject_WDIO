506fa6efe8d2706ea3875e99fe682da6beae0fc1
HarryHoudini <englishmanrt@gmail.com>
2019-03-10 00:34:09 +0300

    start

---
+++ test/no_po.ts
@@ -0,0 +1,11 @@
+import {expect} from 'chai';
+
+describe('Guest', function () {
+    it('should be able to buy item',  function() {
+        browser.url('/rubber-ducks-c-1/red-duck-p-3');
+        const quantitoCard = $("span.quantity")
+        
+
+
+    })
+})
