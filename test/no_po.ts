687de12b178755a6df35df41adf16cc3efde3776
HarryHoudini <englishmanrt@gmail.com>
2019-03-10 01:25:52 +0300

    half fill customer details

--- test/no_po.ts
+++ test/no_po.ts
@@ -1,11 +1,24 @@
 import {expect} from 'chai';
+import * as faker from 'faker'
 
 describe('Guest', function () {
     it('should be able to buy item',  function() {
         browser.url('/rubber-ducks-c-1/red-duck-p-3');
-        const quantitoCard = $("span.quantity")
-        
-
-
+        const quantityItmBeforeAddToCard = $("span.quantity").getText()
+        const quantitySumBeforeAddToCard = $("span.formatted_value").getText().slice(1)
+        $('button.btn-success').click()
+        browser.pause(1500) //bad
+        const quantityItmAftAddToCard = $("span.quantity").getText()
+        const quantitySumAftAddToCard = $("span.formatted_value").getText().slice(1)
+        expect( +quantityItmAftAddToCard).to.equal( +quantityItmBeforeAddToCard + 1)
+        expect(+quantitySumAftAddToCard).to.equal(  +quantitySumBeforeAddToCard + +quantitySumAftAddToCard)
+        $('#cart').click()
+        $('[name="company"]').setValue('CompanyName')
+        $('[name="firstname"]').setValue('firstname')
+        $('[name="lastname"]').setValue('lastname')
+        $('[name="city"]').setValue('city')
+        $('[name="postcode"]').setValue(faker.address.zipCode())
+        $('[name="address1"]').setValue(faker.address.streetAddress)
+        $('[name="address2"]').setValue(faker.address.streetAddress)
     })
 })
--- wdio.conf.js
+++ wdio.conf.js
@@ -23,7 +23,7 @@ exports.config = {
     //
     sync: true,
     specs: [
-        './test/pinup/landing/register-email.ts'
+        './test/no_po.ts'
     ],
     // Patterns to exclude.
     exclude: [
@@ -81,7 +81,8 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: 'http://the-internet.herokuapp.com',
+    // baseUrl: 'http://the-internet.herokuapp.com',
+    baseUrl: 'http://demo.litecart.net',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
