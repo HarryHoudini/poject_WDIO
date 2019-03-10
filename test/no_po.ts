c61cb7534b58863e53e777d63e1ec33c874f63fc
HarryHoudini <englishmanrt@gmail.com>
2019-03-10 17:26:41 +0300

    no_po_Guest buy item

--- test/no_po.ts
+++ test/no_po.ts
@@ -1,24 +1,56 @@
-import {expect} from 'chai';
-import * as faker from 'faker'
+import { expect } from "chai";
+import * as faker from "faker";
 
-describe('Guest', function () {
-    it('should be able to buy item',  function() {
-        browser.url('/rubber-ducks-c-1/red-duck-p-3');
-        const quantityItmBeforeAddToCard = $("span.quantity").getText()
-        const quantitySumBeforeAddToCard = $("span.formatted_value").getText().slice(1)
-        $('button.btn-success').click()
-        browser.pause(1500) //bad
-        const quantityItmAftAddToCard = $("span.quantity").getText()
-        const quantitySumAftAddToCard = $("span.formatted_value").getText().slice(1)
-        expect( +quantityItmAftAddToCard).to.equal( +quantityItmBeforeAddToCard + 1)
-        expect(+quantitySumAftAddToCard).to.equal(  +quantitySumBeforeAddToCard + +quantitySumAftAddToCard)
-        $('#cart').click()
-        $('[name="company"]').setValue('CompanyName')
-        $('[name="firstname"]').setValue('firstname')
-        $('[name="lastname"]').setValue('lastname')
-        $('[name="city"]').setValue('city')
-        $('[name="postcode"]').setValue(faker.address.zipCode())
-        $('[name="address1"]').setValue(faker.address.streetAddress)
-        $('[name="address2"]').setValue(faker.address.streetAddress)
-    })
-})
+describe("Guest", function() {
+  it("should be able to buy item", function() {
+    browser.url("/rubber-ducks-c-1/red-duck-p-3");
+    const quantityItmBeforeAddToCard = Number($("span.quantity").getText());
+    const quantitySumBeforeAddToCard = Number(
+      $("span.formatted_value")
+        .getText()
+        .slice(1)
+    );
+    $("button.btn-success").click();
+    browser.pause(1500); //bad
+    const quantityItmAftAddToCard = Number($("span.quantity").getText());
+    const quantitySumAftAddToCard = Number(
+      $("span.formatted_value")
+        .getText()
+        .slice(1)
+    );
+    expect(quantityItmAftAddToCard).to.equal(quantityItmBeforeAddToCard + 1);
+    expect(quantitySumAftAddToCard).to.equal(
+      quantitySumBeforeAddToCard + quantitySumAftAddToCard
+    );
+    $("#cart").click();
+    $('[name="company"]').setValue("CompanyName");
+    $('[name="firstname"]').setValue("firstname");
+    $('[name="lastname"]').setValue("lastname");
+    $('[name="city"]').setValue("city");
+    $('[name="postcode"]').setValue(faker.address.zipCode());
+    $('[name="address1"]').setValue(faker.address.streetAddress);
+    $('[name="address2"]').setValue(faker.address.streetAddress);
+    $('[name="email"]').setValue(faker.internet.email);
+    $('[name="phone"]').setValue(faker.phone);
+    $('[name="save_customer_details"][disabled="disabled"]').waitForExist(
+      1500,
+      true,
+      "Wait element NOT exist in dome"
+    ); //wait for NOT exist
+
+    browser.waitUntil(
+      () => $('button[name="confirm_order"]').getAttribute("disabled") == null,
+      undefined,
+      "Confirm order button should become enabled to click"
+    );
+    $('button[name="confirm_order"]').click();
+    $("#box-order-success>.title").waitForDisplayed(
+      5000,
+      false,
+      "Order is completed"
+    );
+    expect($("#box-order-success>.title")).to.match(
+      /Your order #[1-9]{1,3} is successfully completed!/
+    );
+  });
+});
