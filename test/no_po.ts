243103659e8dabb10dbc9d0dc5fb8ad86b14bccd
HarryHoudini <englishmanrt@gmail.com>
2019-03-10 18:00:50 +0300

    fix conformation text

--- test/no_po.ts
+++ test/no_po.ts
@@ -23,21 +23,22 @@ describe("Guest", function() {
       quantitySumBeforeAddToCard + quantitySumAftAddToCard
     );
     $("#cart").click();
-    $('[name="company"]').setValue("CompanyName");
-    $('[name="firstname"]').setValue("firstname");
-    $('[name="lastname"]').setValue("lastname");
-    $('[name="city"]').setValue("city");
-    $('[name="postcode"]').setValue(faker.address.zipCode());
+    // $('[name="company"]').setValue("CompanyName");
+    // $('input[name="address2"]').setValue(faker.address.streetAddress());
+    // $('input[name="tax_id"]').setValue(faker.random.number(6));
+    $('input[name="firstname"]').setValue("firstname");
+    $('input[name="lastname"]').setValue("lastname");
+    $('input[name="city"]').setValue("city");
+    $('input[name="postcode"]').setValue("345624");
     $('input[name="address1"]').setValue(faker.address.streetAddress());
-    $('input[name="address2"]').setValue(faker.address.streetAddress());
     $('[name="email"]').setValue(faker.internet.email());
-    $('[name="phone"]').setValue(faker.phone.phoneNumber());
+    $('[name="phone"]').setValue(faker.phone.phoneNumberFormat());
     $('[name="save_customer_details"][disabled="disabled"]').waitForExist(
       1500,
       true,
       "Wait element NOT exist in dome"
     ); //wait for NOT exist
-
+    $('button[name="save_customer_details"]').click();
     browser.waitUntil(
       () => $('button[name="confirm_order"]').getAttribute("disabled") == null,
       undefined,
@@ -45,11 +46,12 @@ describe("Guest", function() {
     );
     $('button[name="confirm_order"]').click();
     $("#box-order-success>.title").waitForDisplayed(
-      5000,
+      7000,
       false,
       "Order is completed"
     );
-    expect($("#box-order-success>.title")).to.match(
+    const conformationText = $("#box-order-success>.title").getText();
+    expect(conformationText).to.match(
       /Your order #[1-9]{1,3} is successfully completed!/
     );
   });
