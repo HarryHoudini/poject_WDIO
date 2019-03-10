7b377442d99f2aef9bc7be0a4a9e364355ecb90d
HarryHoudini <englishmanrt@gmail.com>
2019-03-10 18:48:39 +0300

    add PO file

---
+++ PageObjects/index.ts
@@ -0,0 +1,3 @@
+export { ProductDetails } from '../PageObjects/productDetails'
+export { Checkout } from '../PageObjects/checkout'
+export { Confirmation } from '../PageObjects/confirmation'
--- test/po.ts
+++ test/po.ts
@@ -1,5 +1,7 @@
 import { expect } from "chai";
 import * as faker from "faker";
+import { ProductDetails, Checkout, Confirmation} from "../PageObjects"
+
 describe("Guest", function() {
   it("should be able to buy item", function() {
     browser.url("/rubber-ducks-c-1/red-duck-p-3");
